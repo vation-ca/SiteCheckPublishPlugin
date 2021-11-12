7603eb7ec301d21e79920c325b2720a02299c156
steve-h <stevelist@hume.ca>
2021-11-12 15:13:19 -0500

    Update Package.swift
    up issue with Publish concurrency branch dependency

--- Package.swift
+++ Package.swift
@@ -1,10 +1,12 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SiteCheckPublishPlugin",
+    platforms: [.macOS(.v12)], // Temporary until Xcode 13.2 has been released
+
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
 .package(name: "SwiftSoup",url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.5"),
-.package(name: "Publish",url:"https://github.com/johnsundell/publish.git", from: "0.8.0"),
+.package(name: "Publish",url:"https://github.com/johnsundell/publish.git", .branch("swift-concurrency")), // from: "0.8.0"),
 .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0")
 
     ],
